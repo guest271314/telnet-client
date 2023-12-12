7442b33dd31af2d62261dd20f4ffac54725ca1c3
guest271314 <guest271314@gmail.com>
2023-12-11 23:34:54 -0800

    Update txikijs_echo_tcp.js

--- direct-sockets/txikijs_echo_tcp.js
+++ direct-sockets/txikijs_echo_tcp.js
@@ -4,29 +4,86 @@
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-const listener = await tjs.listen('tcp', '127.0.0.1', '8000');
-const {family, ip, port} = listener.localAddress;
-console.log(`Listening on family: ${family}, ip: ${ip}, port: ${port}`);
+async function readFullAsync(length) {
+  const buffer = new Uint8Array(65536);
+  const data = [];
+  while (data.length < length) {
+    const n = await tjs.stdin.read(buffer);
+    if (n === null) {
+      break;
+    }
+    data.push(...buffer.subarray(0, n));
+  }
+  return new Uint8Array(data);
+}
+
+async function getMessage() {
+  const header = new Uint8Array(4);
+  await tjs.stdin.read(header);
+  const [length] = new Uint32Array(
+    header.buffer,
+  );
+  const output = await readFullAsync(length);
+  return output;
+}
+
+async function sendMessage(message) {
+  // https://stackoverflow.com/a/24777120
+  const header = Uint32Array.from({
+    length: 4,
+  }, (_, index) => (message.length >> (index * 8)) & 0xff);
+  const output = new Uint8Array(header.length + message.length);
+  output.set(header, 0);
+  output.set(message, 4);
+  await tjs.stdout.write(output);
+  return true;
+}
+
+function encodeMessage(message) {
+  return encoder.encode(JSON.stringify(message));
+}
+
+const listener = await tjs.listen("tcp", "127.0.0.1", "8000");
+const { family, ip, port } = listener.localAddress;
+sendMessage(
+  encodeMessage(`Listening on family: ${family}, ip: ${ip}, port: ${port}`),
+);
 try {
   const conn = await listener.accept();
   const writer = conn.writable.getWriter();
 
-  await conn.readable.pipeTo(new WritableStream({
-    async write(value, controller) {
-      const data = decoder.decode(value);
-      console.log(data);
-      await writer.write(encoder.encode(data.toUpperCase()));
-    },
-    close() {
-      console.log('Stream closed');
-    },
-    abort(reason) {
-      console.log(reason);
-    }
-  })).then(() => console.log('Stream aborted')).catch((e) => {
+  await conn.readable.pipeTo(
+    new WritableStream({
+      async write(value, controller) {
+        const data = decoder.decode(value);
+        await writer.write(encoder.encode(data.toUpperCase()));
+      },
+      close() {
+        sendMessage(encodeMessage("Stream closed"));
+      },
+      abort(reason) {
+        sendMesage(encodeMessage(reason));
+      },
+    }),
+  ).then(() => sendMessage(encodeMessage("Stream aborted"))).catch((e) => {
     throw e;
   });
 } catch (e) {
-  console.log(e);
+  sendMesage(encodeMessage(e.message));
   listener.close();
 }
+
+async function main() {
+  while (true) {
+    try {
+      const message = await getMessage();
+      await sendMessage(message);
+      gc();
+    } catch (e) {
+      sendMesage(encodeMessage(e.message));
+      tjs.exit();
+    }
+  }
+}
+
+main();
