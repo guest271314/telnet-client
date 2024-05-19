38f081356403f6d5c54c12273ce5cfdf68d4e57e
guest271314 <guest271314@gmail.com>
2024-05-18 21:44:09 -0700

    Update direct-socket-controller.js

--- direct-sockets/direct-socket-controller.js
+++ direct-sockets/direct-socket-controller.js
@@ -1,65 +1,76 @@
+function setTitle(data) {
+  const title = document.title;
+  document.title = title + data;
+  document.title = title;
+}
+
 var encoder = new TextEncoder();
 var decoder = new TextDecoder();
-var { resolve, reject, promise } = Promise.withResolvers();
+var {resolve, reject, promise} = Promise.withResolvers();
 await navigator.permissions.request({
   name: "notifications",
 });
-new Notification("Open IWA, connect to TCP server?").onclick = async () => {
+new Notification("Open IWA, connect to TCP server?").onclick = async()=>{
   resolve(showSaveFilePicker({
     startIn: "downloads",
     suggestedName: "direct-socket-controller.sdp",
   }));
-};
+}
+;
 var handle = await promise;
-var fso = new FileSystemObserver(
-  async ([{ changedHandle, root, type }], record) => {
-    try {
-      console.log(type);
-      fso.disconnect();
-      fso.unobserve(handle);
-      var text = atob(await (await handle.getFile()).text());
-      await local.setRemoteDescription({
-        type: "answer",
-        sdp: text,
-      });
+var fso = new FileSystemObserver(async([{changedHandle, root, type}],record)=>{
+  try {
+    console.log(type);
+    fso.disconnect();
+    fso.unobserve(handle);
+    var text = atob(await (await handle.getFile()).text());
+    await local.setRemoteDescription({
+      type: "answer",
+      sdp: text,
+    });
 
-      await handle.remove();
-    } catch (e) {
-      console.warn(e);
-    }
-  },
-);
+    await handle.remove();
+  } catch (e) {
+    console.warn(e);
+  }
+}
+,);
 fso.observe(handle);
 var local = new RTCPeerConnection({
   sdpSemantics: "unified-plan",
 });
-[
-  "onsignalingstatechange",
-  "oniceconnectionstatechange",
-  "onicegatheringstatechange",
-].forEach((e) => local.addEventListener(e, console.log));
+["onsignalingstatechange", "oniceconnectionstatechange", "onicegatheringstatechange", ].forEach((e)=>local.addEventListener(e, console.log));
 
-local.onicecandidate = async (e) => {
+local.onicecandidate = async(e)=>{
   if (!e.candidate) {
-    local.localDescription.sdp = local.localDescription.sdp.replace(
-      /actpass/,
-      "active",
-    );
+    local.localDescription.sdp = local.localDescription.sdp.replace(/actpass/, "active", );
     if (local.localDescription.sdp.indexOf("a=end-of-candidates") === -1) {
       local.localDescription.sdp += "a=end-of-candidates\r\n";
     }
     try {
       console.log("sdp:", local.localDescription);
+      // https://issues.chromium.org/issues/339994757#comment6
+      /*
       var w = open(
         `IWA_URL?sdp=${
           btoa(local.localDescription.sdp)
         }`,
       );
+      */
+      const params = new URLSearchParams();
+      params.append("name", "Signed Web Bundle in Isolated Web App");
+      params.append("sdp", btoa(local.localDescription.sdp));
+      if (globalThis?.openIsolatedWebApp) {
+        openIsolatedWebApp(`?${params.toString()}`);
+      } else {
+        setTitle(`?${params.toString()}`);
+      }
     } catch (e) {
       console.error(e);
     }
   }
-};
+}
+;
 var channel = local.createDataChannel("transfer", {
   negotiated: true,
   ordered: true,
@@ -68,19 +79,23 @@ var channel = local.createDataChannel("transfer", {
   protocol: "tcp",
 });
 
-channel.onopen = async (e) => {
+channel.onopen = async(e)=>{
   console.log(e.type);
-};
-channel.onclose = async (e) => {
+}
+;
+channel.onclose = async(e)=>{
   console.log(e.type);
-};
-channel.onclosing = async (e) => {
+}
+;
+channel.onclosing = async(e)=>{
   console.log(e.type);
-};
-channel.onmessage = async (e) => {
+}
+;
+channel.onmessage = async(e)=>{
   // Do stuff with data
   console.log(e.data);
-};
+}
+;
 
 var offer = await local.createOffer({
   voiceActivityDetection: false,
